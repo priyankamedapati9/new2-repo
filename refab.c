a736bd5562a91d47761e7ec0c43633c7de6960ad
priyankamedapati9 <medapatipriyanka2@gmail.com>
2025-09-30 17:55:50 +0530

    Add files via upload

---
+++ refab.c
@@ -0,0 +1,38 @@
+/***********factorial*****
+#include<stdio.h>
+int facto(int n);
+void main(){
+int f;
+f=facto(5);
+printf("%d",f);
+}
+int facto(int n){
+if(n==1)
+return 1;
+else
+return n*facto(n-1);
+}*/
+
+
+
+#include<stdio.h>
+int fab(int);
+void main()
+{
+int n,a=0,i;
+printf("enter");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%d",fab(i));
+printf("hello");}
+printf("conn");
+}
+int fab(int a)
+{
+if(a<2)
+{
+return 1;
+}
+return(fab(a-1)+fab(a-2));
+}
